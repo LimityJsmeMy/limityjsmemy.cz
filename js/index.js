cabcc26642b58ebff6be9abe014a80b1ac5d14f0
aighul-dhabati <aighul-dhabati@users.noreply.github.com>
2021-02-20 20:43:41 +0000

    deploy: 154b963d6d71367e672c83fc0d87d3d5dcdc232c

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-var articleTeasers = document.querySelectorAll('.teaser-wrapper');
-Array.prototype.forEach.call(articleTeasers, function (el, i) {
+var wrappers = document.querySelectorAll('.teaser-wrapper, .featured-wrapper');
+Array.prototype.forEach.call(wrappers, function (el, i) {
   var tilt = -0.25 + Math.random() / 2;
   el.style.transform = 'rotate(' + tilt + 'deg)';
   let firstChild = el.children[0];
   firstChild.style.transform = 'rotate(-' + tilt + 'deg)';
 });
-
 
